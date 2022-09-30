3e96c6371824b05bc605fb12a17ac54e957fdb5e
Nicolás Jurado <nicomacojurado@gmail.com>
2022-09-29 19:00:30 -0500

    Ahora sirve y corre el JavaFX
    Hubo unos cambios pequeños y direcciones de los archivos

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,28 +2,29 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
 
-            Scene scene = new Scene(root);
+	public static void main(String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainView.fxml"));
+		try {
+			//Parent root = FXMLLoader.load(getClass().getResource(""));
+            Scene scene = new Scene(fxmlLoader.load(), 500, 500);
             primaryStage.setScene(scene);
             primaryStage.show();
-			primaryStage.show();
-		} catch(Exception e) {
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module SubastasQuindio {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
-	
+
+	opens controllers to javafx.fxml;
 	opens application to javafx.graphics, javafx.fxml;
+	exports application;
 }
